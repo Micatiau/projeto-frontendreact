6d50bf02c2866f8e997adca8b20afcb23c13100f
Micaeli Takara Becca <Mikaelitakara@gmail.com>
2023-05-29 01:27:15 -0300

    Finalizado

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -1,9 +1,10 @@
 import { HeaderContainer, Logo } from "./HeaderStyle"
 
+
 function Header () {
     return(
         <HeaderContainer>
-        <Logo src="../src/data/images/SPACE_DREAM.png" alt="logo" />
+        <Logo src="../src/data/SPACE_DREAM.png" alt="logo" />
         </HeaderContainer>
     )
 }
