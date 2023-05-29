4a670c42576434e9be636bd80b8d649750bec7d3
Micaeli Takara Becca <Mikaelitakara@gmail.com>
2023-05-29 00:53:07 -0300

    Finalizado

--- src/data/productList.js
+++ src/data/productList.js
@@ -1,58 +1,67 @@
+import Cat from './images/Cat.png'
+import Stuck from './images/Stuck.png'
+import Sweet from './images/Sweet.png'
+import Puppy from './images/Puppy.png'
+import Baby from './images/Baby.png'
+import Lamb from './images/Lamb.png'
+import Moose from './images/Moose.png'
+import BrakeDance from './images/BrakeDance.png'
+import ShowTV from './images/ShowTV.png'
 
 export const results = [
   {
     id: 1,
     title: 'Camisa Cat',
     price: 50.25,
-    image: '../src/assets/Cat.png',
+    image: Cat,
   },
   {
     id: 2,
     title: 'Camisa Stuck ',
     price: 71.50,
-    image: '../src/data/Stuck.png',
+    image: Stuck,
   },
   {
     id: 3,
     title: 'Camisa Sweet',
     price: 102.75,
-    image: '../src/data/Sweet.png',
+    image: Sweet,
   },
   {
     id: 4,
     title: 'Camisa Puppy',
     price: 138.95,
-    image: '../src/data/Puppy.png',
+    image: Puppy,
   },
   {
     id: 5,
     title:
       'Camisa Hi Baby!',
     price: 56.70,
-    image: '../src/data/Baby.png',
+    image: Baby,
   },
   {
     id: 6,
     title: 'Camisa Lamb',
     price: 86.90,
-    image: '../src/data/Lamb.png',
+    image: Lamb,
   },
   {
     id: 7,
     title: 'Camisa Moose',
     price: 117.80,
-    image: '../src/data/Moose.png',
+    image: Moose,
   },
   {
     id: 8,
     title: 'Camisa Brake Dance',
     price: 63.15,
-    image: '../src/data/BrakeDance.png',
+    image: BrakeDance,
   },
   {
     id: 9,
     title: 'Camisa Show TV',
     price: 94.40,
-    image: '../src/data/ShowTV.png',
+    image: ShowTV,
   },
 ];
